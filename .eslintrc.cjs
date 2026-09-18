4fc973b65b3e261fb6acb60b29ffc1f50c3ef5d6
Jessdanie94 <jessesdigitalventures@gmail.com>
2026-09-18 11:54:15 -0600

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,3 @@
-/** @type {import("@types/eslint").Linter.BaseConfig} */
-module.exports = {
-  root: true,
-  extends: [
-    "@remix-run/eslint-config",
-    "@remix-run/eslint-config/node",
-    "@remix-run/eslint-config/jest-testing-library",
-    "prettier",
-  ],
-  settings: {
-    jest: {
-      version: 29,
-    },
-   },
-  globals: {
-    shopify: "readonly",
-  },
-};
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
